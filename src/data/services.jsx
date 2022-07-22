cd1aca672ad4d481b1a85c65398560fe960a49ed
Lazyademola <88086951+Lazyademola@users.noreply.github.com>
2022-07-23 02:36:41 +0800

    First commit Message

---
+++ src/data/services.jsx
@@ -0,0 +1,21 @@
+import {HiAcademicCap} from 'react-icons/hi'
+import {HiCode} from 'react-icons/hi';
+import {HiAnnotation} from 'react-icons/hi';
+
+export default [
+    {
+        title: "Web design",
+        description:"I'm a frontend developer from Lagos,I build web applications using React",
+        icon: <HiAcademicCap className="w-full h-full"/>,
+    },
+    {
+        title: "UI Development",
+        description:"I'm a frontend developer from Lagos,I build web applications using React",
+        icon: <HiCode className="w-full h-full"/>,
+    },
+    {
+        title: "Web Development",
+        description:"I'm a frontend developer from Lagos,I build web applications using React",
+        icon: <HiAnnotation className="w-full h-full"/>,
+    },
+]
---
+++ src/data/works.js
@@ -0,0 +1,14 @@
+export default[
+    {
+        title:'Recipe ' ,
+        imgUrl:'https://raw.githubusercontent.com/Lazyademola/RecipeJs/main/ReadMeImage/readmeimg.jpg',
+        tech:['CSS', 'HTML', 'JavaScript'],
+        workUrl:'https://ounjerecipe2.netlify.app/',
+    },
+    {
+        title:'Portfolio' ,
+        imgUrl:'https://raw.githubusercontent.com/Lazyademola/Practice/main/ReadMeImages/readmeimg1.jpg',
+        tech:['CSS', 'HTML', 'Javascript'],
+        workUrl:'https://naughtymayorportfolio.netlify.app/'
+    },
+]
